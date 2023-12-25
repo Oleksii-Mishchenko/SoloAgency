e1eb47ebbd8d82d2b1bcb82b102cc2d248adbfb3
OleksiiMishchenko <alex.mishch@gmail.com>
2023-12-25 19:01:42 +0200

    Created pages components and routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
+import { Outlet } from 'react-router-dom';
 import './App.scss';
+import { Link } from 'react-router-dom';
 
 export const App = () => {
   return (
     <div className="App">
       <h1 className="App__title">Solo Agency</h1>
+      <Link to={'home'}>Home</Link>
+      <Link to={'about-us'}>About us</Link>
+      <Link to={'faq'}>FAQ</Link>
+      <Link to={'contacts'}>Contacts</Link>
+      <Link to={'services'}>Services</Link>
+      <Link to={'anypage'}>Page not found</Link>
+      <Outlet />
     </div>
   );
 };
---
+++ frontend/src/pages/AboutUsPage/AboutUsPage.tsx
@@ -0,0 +1,9 @@
+export const AboutUsPage = () => {
+  return (
+    <div className="about-us">
+      <div className="about-us-page">About us</div>
+
+      <h1>Hello world</h1>
+    </div>
+  );
+};
