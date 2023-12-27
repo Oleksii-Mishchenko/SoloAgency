d21069c6fe83989192ad3ca14222745c5f3f22ac
Taras Savchyn <savik1992@gmail.com>
2023-12-27 19:55:08 +0200

    install django cors headers

--- backend/solo_agency/settings.py
+++ backend/solo_agency/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "rest_framework",
     "rest_framework.authtoken",
     "user",
@@ -39,6 +40,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -133,3 +135,7 @@ REST_FRAMEWORK = {
     ],
 
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
