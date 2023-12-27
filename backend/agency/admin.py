7defdd40174993eb875255503d5cb220b9e21d49
Taras Savchyn <savik1992@gmail.com>
2023-12-27 21:47:01 +0200

    create admin.py

--- backend/agency/admin.py
+++ backend/agency/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import Service, Agency, EventType, Organizer, Event, Advice, Review
 
-# Register your models here.
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+@admin.register(Agency)
+class AgencyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    filter_horizontal = ('services',)
+
+@admin.register(EventType)
+class EventTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+@admin.register(Organizer)
+class OrganizerAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'position', 'phone', 'email')
+    search_fields = ('first_name', 'last_name', 'position', 'phone', 'email')
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'event_type', 'date', 'style', 'user', 'created_at')
+    filter_horizontal = ('organizers',)
+
+@admin.register(Advice)
+class AdviceAdmin(admin.ModelAdmin):
+    list_display = ('question', 'answer')
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('user', 'text', 'rating', 'created_at', 'updated_at', 'is_approved')
+    list_filter = ('is_approved',)
+    search_fields = ('user__username', 'text')
