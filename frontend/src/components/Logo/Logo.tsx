def34e8bd08b82b80d7a4ceb349d9a2e9a0b0900
OleksiiMishchenko <alex.mishch@gmail.com>
2024-01-02 14:16:04 +0200

    Added Logo component

---
+++ frontend/src/components/Logo/Logo.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logoImage from '../../assets/img/Logo.svg';
+import './_logo.scss';
+
+export const Logo: React.FC = () => {
+  return (
+    <Link to={'/home'} className="logo">
+      <img src={logoImage} alt="Logo" className="logo__image" />
+    </Link>
+  );
+};
