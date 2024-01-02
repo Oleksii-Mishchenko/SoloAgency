59f20a095fda401d42fcb5678a61de7621f0b548
OleksiiMishchenko <alex.mishch@gmail.com>
2024-01-02 18:30:51 +0200

    Added hero section

--- frontend/src/components/MainButton/MainButton.tsx
+++ frontend/src/components/MainButton/MainButton.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './main-button.scss';
+import classNames from 'classnames';
 
 type Props = {
   text: string;
+  className?: string;
 };
 
-export const MainButton: React.FC<Props> = ({ text }) => {
-  return <button className="main-button">{text}</button>;
+export const MainButton: React.FC<Props> = ({ text, className }) => {
+  return (
+    <button className={classNames('main-button', className)}>{text}</button>
+  );
 };
--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -1,123 +1,139 @@
-import { useEffect, useState } from 'react';
-import { useAppDispatch, useAppSelector } from '../../app/hooks';
-import { LoginData } from '../../types/LoginData';
-import * as authActions from '../../features/authSlice';
-import { RegisterData } from '../../types/RegisterData';
-import { Loader2 } from '../../components/Loader2';
+import { MainButton } from '../../components/MainButton';
+import './home-page.scss';
 
 export const HomePage = () => {
-  const [loginData, setLoginData] = useState<LoginData>({
-    email: '',
-    password: '',
-  });
-  const [registerData, setRegisterData] = useState<RegisterData>({
-    email: '',
-    password: '',
-    first_name: '',
-    last_name: '',
-    phone: '',
-  });
-  const dispatch = useAppDispatch();
-  const { authData } = useAppSelector(state => state.auth);
-
-  const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    dispatch(authActions.login(loginData));
-  };
-
-  const handleRegister = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    dispatch(authActions.register(registerData));
-  };
-
-  useEffect(() => {
-    if (authData.token) {
-      dispatch(authActions.getUserByToken(authData.token));
-    }
-  }, [authData.token, dispatch]);
-
   return (
     <div className="home-page">
-      <h2>Home Page</h2>
-      <Loader2 />
-      admin@admin.ua
-      <form onSubmit={event => handleLogin(event)}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={loginData.email}
-          onChange={event =>
-            setLoginData(state => ({ ...state, email: event.target.value }))
-          }
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={loginData.password}
-          onChange={event =>
-            setLoginData(state => ({ ...state, password: event.target.value }))
-          }
-        />
-        <button>Log in</button>
-      </form>
-      <br />
-      <form onSubmit={event => handleRegister(event)}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={registerData.email}
-          onChange={event =>
-            setRegisterData(state => ({ ...state, email: event.target.value }))
-          }
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={registerData.password}
-          onChange={event =>
-            setRegisterData(state => ({
-              ...state,
-              password: event.target.value,
-            }))
-          }
-        />
-        <input
-          type="text"
-          placeholder="First name"
-          value={registerData.first_name}
-          onChange={event =>
-            setRegisterData(state => ({
-              ...state,
-              first_name: event.target.value,
-            }))
-          }
-        />
-        <input
-          type="text"
-          placeholder="Last name"
-          value={registerData.last_name}
-          onChange={event =>
-            setRegisterData(state => ({
-              ...state,
-              last_name: event.target.value,
-            }))
-          }
-        />
-        <input
-          type="tel"
-          placeholder="Phone number"
-          value={registerData.phone}
-          onChange={event =>
-            setRegisterData(state => ({
-              ...state,
-              phone: event.target.value,
-            }))
-          }
-        />
-        <button>Register</button>
-      </form>
+      <section className="home-page__hero">
+        <div className="home-page__hero-content">
+          <p className="home-page__hero-decorative-text">
+            Організовуємо мрії та перетворюємо їх у реальність - ваші події,
+            наша справа!
+          </p>
+
+          <MainButton
+            text="Замовити послугу"
+            className="home-page__hero-button"
+          />
+        </div>
+      </section>
     </div>
   );
 };
+
+// export const HomePage = () => {
+//   const [loginData, setLoginData] = useState<LoginData>({
+//     email: '',
+//     password: '',
+//   });
+//   const [registerData, setRegisterData] = useState<RegisterData>({
+//     email: '',
+//     password: '',
+//     first_name: '',
+//     last_name: '',
+//     phone: '',
+//   });
+//   const dispatch = useAppDispatch();
+//   const { authData } = useAppSelector(state => state.auth);
+
+//   const handleLogin = (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+
+//     dispatch(authActions.login(loginData));
+//   };
+
+//   const handleRegister = (event: React.FormEvent<HTMLFormElement>) => {
+//     event.preventDefault();
+
+//     dispatch(authActions.register(registerData));
+//   };
+
+//   useEffect(() => {
+//     if (authData.token) {
+//       dispatch(authActions.getUserByToken(authData.token));
+//     }
+//   }, [authData.token, dispatch]);
+
+//   return (
+//     <div className="home-page">
+//       <h2>Home Page</h2>
+//       <Loader2 />
+//       admin@admin.ua
+//       <form onSubmit={event => handleLogin(event)}>
+//         <input
+//           type="email"
+//           placeholder="Email"
+//           value={loginData.email}
+//           onChange={event =>
+//             setLoginData(state => ({ ...state, email: event.target.value }))
+//           }
+//         />
+//         <input
+//           type="password"
+//           placeholder="Password"
+//           value={loginData.password}
+//           onChange={event =>
+//             setLoginData(state => ({ ...state, password: event.target.value }))
+//           }
+//         />
+//         <button>Log in</button>
+//       </form>
+//       <br />
+//       <form onSubmit={event => handleRegister(event)}>
+//         <input
+//           type="email"
+//           placeholder="Email"
+//           value={registerData.email}
+//           onChange={event =>
+//             setRegisterData(state => ({ ...state, email: event.target.value }))
+//           }
+//         />
+//         <input
+//           type="password"
+//           placeholder="Password"
+//           value={registerData.password}
+//           onChange={event =>
+//             setRegisterData(state => ({
+//               ...state,
+//               password: event.target.value,
+//             }))
+//           }
+//         />
+//         <input
+//           type="text"
+//           placeholder="First name"
+//           value={registerData.first_name}
+//           onChange={event =>
+//             setRegisterData(state => ({
+//               ...state,
+//               first_name: event.target.value,
+//             }))
+//           }
+//         />
+//         <input
+//           type="text"
+//           placeholder="Last name"
+//           value={registerData.last_name}
+//           onChange={event =>
+//             setRegisterData(state => ({
+//               ...state,
+//               last_name: event.target.value,
+//             }))
+//           }
+//         />
+//         <input
+//           type="tel"
+//           placeholder="Phone number"
+//           value={registerData.phone}
+//           onChange={event =>
+//             setRegisterData(state => ({
+//               ...state,
+//               phone: event.target.value,
+//             }))
+//           }
+//         />
+//         <button>Register</button>
+//       </form>
+//     </div>
+//   );
+// };
