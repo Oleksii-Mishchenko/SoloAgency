09fb01609b3014dc2d4268cd86361840aa66c3cc
OleksiiMishchenko <alex.mishch@gmail.com>
2024-01-04 00:35:35 +0200

    Added new Loader

--- frontend/src/components/MainButton/MainButton.tsx
+++ frontend/src/components/MainButton/MainButton.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import './main-button.scss';
 import classNames from 'classnames';
+import { Loader } from '../Loader';
 
 type Props = {
   text: string;
   className?: string;
+  isLoading?: boolean;
 };
 
-export const MainButton: React.FC<Props> = ({ text, className }) => {
+export const MainButton: React.FC<Props> = ({ text, className, isLoading }) => {
   return (
-    <button className={classNames('main-button', className)}>{text}</button>
+    <button className={classNames('main-button', className)}>
+      {text}
+      {isLoading && (
+        <div className="main-button__loader-container">
+          <Loader element="button" />
+        </div>
+      )}
+    </button>
   );
 };
