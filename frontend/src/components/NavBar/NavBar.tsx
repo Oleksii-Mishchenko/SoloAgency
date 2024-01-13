6669f12373bbb3f672f299eee04260c145cf296e
OleksiiMishchenko <alex.mishch@gmail.com>
2024-01-13 15:50:44 +0200

    Header minor fixes

--- frontend/src/components/NavBar/NavBar.tsx
+++ frontend/src/components/NavBar/NavBar.tsx
@@ -3,13 +3,20 @@ import { HeaderNavLink } from '../HeaderNavLink';
 import { navLinks } from '../../assets/navLinks/navLinks';
 import './nav-bar.scss';
 
-export const NavBar: React.FC = () => {
+type Props = {
+  menu: {
+    isMenuOpen: boolean;
+    toggleMenu: () => void;
+  };
+};
+
+export const NavBar: React.FC<Props> = ({ menu }) => {
   return (
     <nav className="nav-bar">
       <ul className="nav-bar__list">
         {navLinks.map(({ id, path, text }) => (
           <li className="nav-bar__item" key={id}>
-            <HeaderNavLink path={path} text={text} />
+            <HeaderNavLink path={path} text={text} menu={menu} />
           </li>
         ))}
       </ul>
