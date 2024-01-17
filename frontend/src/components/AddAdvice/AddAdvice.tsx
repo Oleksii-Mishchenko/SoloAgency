ea1c4b9708d2e87b39081a60cbfef13ce6714c72
OleksiiMishchenko <alex.mishch@gmail.com>
2024-01-17 18:39:50 +0200

    Added Add advice

--- frontend/src/components/AddAdvice/AddAdvice.tsx
+++ frontend/src/components/AddAdvice/AddAdvice.tsx
@@ -4,7 +4,11 @@ import * as advicesActions from '../../features/advicesSlice';
 import { useAppDispatch } from '../../app/hooks';
 import './add-advice.scss';
 
-export const AddAdvice = () => {
+type Props = {
+  relPage: string;
+};
+
+export const AddAdvice: React.FC<Props> = ({ relPage }) => {
   const dispatch = useAppDispatch();
   const [newAdvice, setNewAdvice] = useState<NewAdvice>({
     question: '',
@@ -19,30 +23,32 @@ export const AddAdvice = () => {
   };
 
   return (
-    <form onSubmit={handleAdd}>
-      <input
-        type="text"
-        placeholder="Question"
-        value={newAdvice.question}
-        onChange={event =>
-          setNewAdvice(state => ({
-            ...state,
-            question: event.target.value,
-          }))
-        }
-      />
-      <input
-        type="text"
-        placeholder="Answer"
-        value={newAdvice.answer}
-        onChange={event =>
-          setNewAdvice(state => ({
-            ...state,
-            answer: event.target.value,
-          }))
-        }
-      />
-      <button>Add</button>
-    </form>
+    <section className={`${relPage}__add-advice add-advice`}>
+      <form onSubmit={handleAdd}>
+        <input
+          type="text"
+          placeholder="Question"
+          value={newAdvice.question}
+          onChange={event =>
+            setNewAdvice(state => ({
+              ...state,
+              question: event.target.value,
+            }))
+          }
+        />
+        <input
+          type="text"
+          placeholder="Answer"
+          value={newAdvice.answer}
+          onChange={event =>
+            setNewAdvice(state => ({
+              ...state,
+              answer: event.target.value,
+            }))
+          }
+        />
+        <button>Add</button>
+      </form>
+    </section>
   );
 };
--- frontend/src/pages/AdvicesPage/AdvicesPage.tsx
+++ frontend/src/pages/AdvicesPage/AdvicesPage.tsx
@@ -1,13 +1,13 @@
 import './advices-page.scss';
 import { Advices } from '../../components/Advices';
-import { AddAdvice } from '../../components/AddAdvice';
+// import { AddAdvice } from '../../components/AddAdvice';
 
 export const AdvicesPage = () => {
   return (
     <div className="advices-page">
       <Advices relPage="advices-page" />
 
-      <AddAdvice />
+      {/* <AddAdvice relPage="advices-page" /> */}
     </div>
   );
 };
