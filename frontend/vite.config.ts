f7323c235be442de0fc88d73c90d6fd53d0cbe3f
Taras Savchyn <savik1992@gmail.com>
2024-01-31 20:22:14 +0200

    comment frontend in docker-compose.yml

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/SoloAgency/',
-  // server: {
-  //   host: '0.0.0.0',
-  //   port: 3000,
-  // },
+  server: {
+    host: '0.0.0.0',
+    port: 3000,
+  },
   plugins: [react()],
 });
