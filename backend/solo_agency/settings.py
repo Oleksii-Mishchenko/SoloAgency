45497037524b6793cd0548157c8cc71a12f2faec
Taras Savchyn <savik1992@gmail.com>
2024-02-08 22:41:38 +0200

    add SECRET_KEY to .env

--- backend/solo_agency/settings.py
+++ backend/solo_agency/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-alkhrgl9bh+e2r256(w!#dk!wynfv2*6o9i5z7k%8le*0u0l5!"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
