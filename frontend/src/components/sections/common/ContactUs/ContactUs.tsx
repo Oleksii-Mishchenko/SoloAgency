21c9b6996224098f09b56abaa3077376f71daf83
OleksiiMishchenko <alex.mishch@gmail.com>
2024-03-07 09:07:14 +0200

    Changed contact-us links

--- frontend/src/components/sections/common/ContactUs/ContactUs.tsx
+++ frontend/src/components/sections/common/ContactUs/ContactUs.tsx
@@ -11,14 +11,24 @@ export const ContactUs: React.FC<Props> = ({ relPage }) => {
       <div className="contact-us__contact">
         <p className="contact-us__contact-name">Контактні номери телефонів</p>
 
-        <p className="contact-us__contact-value">+38 (063) 028 28 13</p>
-        <p className="contact-us__contact-value">+38 (093) 816 16 54</p>
+        <a href="tel:+380630282813" className="contact-us__contact-value">
+          +38 (063) 028 28 13
+        </a>
+
+        <a href="tel:+380938161654" className="contact-us__contact-value">
+          +38 (093) 816 16 54
+        </a>
       </div>
 
       <div className="contact-us__contact">
         <p className="contact-us__contact-name">Електронна адреса</p>
 
-        <p className="contact-us__contact-value">soloagency2000@gmail.com</p>
+        <a
+          href="mailto:soloagency2000@gmail.com"
+          className="contact-us__contact-value"
+        >
+          soloagency2000@gmail.com
+        </a>
       </div>
 
       <div className="contact-us__contact">
